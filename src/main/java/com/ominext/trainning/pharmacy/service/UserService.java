0e4c86a2471fe94a79e66a6a9cc58dfff484fc78
DAO MANH TOAN <toandm@ominext.com>
2022-12-20 15:10:56 +0700

    implement request model + service interface

---
+++ src/main/java/com/ominext/trainning/pharmacy/service/UserService.java
@@ -0,0 +1,9 @@
+package com.ominext.trainning.pharmacy.service;
+
+import com.ominext.trainning.pharmacy.request.UserRegistrationDto;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+  User save(UserRegistrationDto registrationDto);
+}
